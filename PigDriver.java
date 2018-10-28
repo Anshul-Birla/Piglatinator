4d0b568da16062563730235aaaabd0d5ccfa0309
Anshul Birla <anshulbirla@gmail.com>
2018-10-28 12:16:44 -0700

    added user control for amount of inputs

--- PigDriver.java
+++ PigDriver.java
@@ -10,13 +10,32 @@ public class PigDriver
     public static void main(String args[])
     {
         ConsoleIO keyboard = new ConsoleIO();
-        String input;
+        String input, contin = "y";
+        
+        
         
         System.out.println("This turns your English phrase into PigLatin!");
-        System.out.print("Enter the phrase you want to be translated:");
+        System.out.println("Note: Enter y if you wish to continue at the end");
+        System.out.print("\n");
+        while (contin.equals("y"))
+        {
+        System.out.print("\n");
+        System.out.print("Enter the phrase you want to be translated and press <Enter>: ");
+        System.out.print("\n");
+        
         input = keyboard.readLine();
+        System.out.print("\n");
         PigLatinator pig = new PigLatinator(input);
         
-        System.out.print("PigLatin - "+pig.run());
+        System.out.print("PigLatin: "+pig.run());
+        System.out.print("\n");
+        System.out.print("\n");
+        
+        System.out.print("Do you wish to continue? ");
+        contin = keyboard.readToken();
     }
+    
+        
+    
+}
 }
