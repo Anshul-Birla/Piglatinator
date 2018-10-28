51ecb817e8d1824605b43e512c1ab1a0173a1b19
Anshul Birla <anshulbirla@gmail.com>
2018-10-28 12:36:51 -0700

    commenting in driver class

--- PigDriver.java
+++ PigDriver.java
@@ -9,28 +9,39 @@ public class PigDriver
 {
     public static void main(String args[])
     {
+        //initialization 
         ConsoleIO keyboard = new ConsoleIO();
-        String input, contin = "y";
+        String input;
+        String contin = "y";
         
-        
-        
+        //instruction to user
         System.out.println("This turns your English phrase into PigLatin!");
         System.out.println("Note: Enter y if you wish to continue at the end");
+        
         System.out.print("\n");
+        
+        //loop to give control to user for program
         while (contin.equals("y"))
         {
         System.out.print("\n");
+        
+        //accept user input
         System.out.print("Enter the phrase you want to be translated and press <Enter>: ");
         System.out.print("\n");
         
         input = keyboard.readLine();
+        
         System.out.print("\n");
+        
+        //constructor initialized later because of necessary info
         PigLatinator pig = new PigLatinator(input);
         
+        //output to user
         System.out.print("PigLatin: "+pig.run());
         System.out.print("\n");
         System.out.print("\n");
         
+        //prompt to keep program running
         System.out.print("Do you wish to continue? ");
         contin = keyboard.readToken();
     }
